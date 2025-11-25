b504f30effefa2345c0827c539c95fd456bdf942
Ari Cummings <ari@aricummings.com>
2025-11-25 14:44:01 -0800

    nearly done

--- RiddleSite/ViewModels/MainViewModel.cs
+++ RiddleSite/ViewModels/MainViewModel.cs
@@ -4,4 +4,8 @@ namespace RiddleSite.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    [ObservableProperty] public static bool showVerifyQuestion = false;
+    [ObservableProperty] public static bool showRiddle = false;
+    [ObservableProperty] public static string verifyAnswer1 = "";
+    [ObservableProperty] public static string verifyAnswer2 = "";
 }
--- RiddleSite/Views/MainView.axaml.cs
+++ RiddleSite/Views/MainView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using RiddleSite;
+using RiddleSite.ViewModels;
 
 namespace RiddleSite.Views;
 
@@ -8,4 +11,17 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
     }
+    private void GetRiddleButton(object? sender, RoutedEventArgs e)
+    {
+        MainViewModel.showVerifyQuestion = true;
+    }
+
+    private void VerifyAlbaButton(object? sender, RoutedEventArgs e)
+    {
+        if (MainViewModel.verifyAnswer1 == "10/30/2025" && (MainViewModel.verifyAnswer2 == "sweetie" || MainViewModel.verifyAnswer2 == "Sweetie" || MainViewModel.verifyAnswer2 == "sweetheart" || MainViewModel.verifyAnswer2 == "Sweetheart"))
+        {
+            MainViewModel.showRiddle = true;
+        } 
+    }
+    
 }
